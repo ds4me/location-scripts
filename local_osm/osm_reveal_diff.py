174eca3defbf8c456bd6dc74cfdd949669f81984
Elijah Filip <elijahfilip@gmail.com>
2021-03-10 09:58:20 +0700

    Add osmid to the table

--- local_osm/osm_reveal_diff.py
+++ local_osm/osm_reveal_diff.py
@@ -31,18 +31,18 @@ for index, row in osm.iterrows():
     revealMatch = reveal[reveal.externalId == row.externalId]
 
     if len(revealMatch) == 0:
-        notInReveal.append({'externalId': row.externalId, 'lastEdited': row.last_edit_date, 'lastEditedBy': row.last_edit_user})
+        notInReveal.append({'wayId': row.osmid, 'externalId': row.externalId, 'lastEdited': row.last_edit_date, 'lastEditedBy': row.last_edit_user})
 
     elif len(revealMatch) == 1:
         percentCoverage = row.geometry.area / revealMatch.geometry.area.values[0]
         if percentCoverage < .99 or percentCoverage > 1.01:
             # print(f'Difference in area between OSM and Reveal for externalId {row.externalId} = {percentCoverage}')
-            modified.append({'externalId': row.externalId, 'percentAreaMatch': percentCoverage, 'lastEdited': row.last_edit_date, 'lastEditedBy': row.last_edit_user})
+            modified.append({'wayId': row.osmid, 'externalId': row.externalId, 'percentAreaMatch': percentCoverage, 'lastEdited': row.last_edit_date, 'lastEditedBy': row.last_edit_user})
 
     else:
         # print(f'There are {len(revealMatch)} matches for externalId {row.externalId}...')
         # print(revealMatch)
-        revealDupes.append({'externalId': row.externalId, 'revealIds': [revealMatch[['id']].to_list()]})
+        revealDupes.append({'wayId': row.osmid, 'externalId': row.externalId, 'revealIds': [revealMatch[['id']].to_list()]})
 
 # Print the results
 pd.set_option('display.max_rows', None)
